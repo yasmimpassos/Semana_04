f2b0759c3da9c1b546d34653600c700c13d00179
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2024-03-02 19:03:03 -0300

    Implementação da movimentação do playerAzul

--- src/pong.js
+++ src/pong.js
@@ -22,19 +22,41 @@ class Pong extends Phaser.Scene {
 
     create(){
         this.add.image(400, 300, "ceu");
-        this.add.image(50, 300, "playerAzul").setAngle(-90).setScale(0.7);
-        this.add.image(750, 300, "playerRosa").setAngle(90).setScale(0.7);
+
+        this.pontuacaoAzul = 0;
+        this.pontuacaoRosa = 0;
+
+        this.playerAzul = this.add.image(50, 300, "playerAzul").setAngle(-90).setScale(0.7);
+        this.playerRosa = this.add.image(750, 300, "playerRosa").setAngle(90).setScale(0.7);
+
+        // this.playerAzul.setCollideWorldBounds(true);
+        // this.playerRosa.setCollideWorldBounds(true);
+
         this.add.image(200, 50, "estrelaAzul").setScale(0.8);
         this.add.image(600, 50, "estrelaRosa").setScale(0.8);
+
+        this.placarAzul = this.add.text(190, 35, this.pontuacaoAzul, {fontSize: '35px', fill: '#ffffff'});
+        this.placarRosa = this.add.text(590, 35, this.pontuacaoRosa, {fontSize: '35px', fill: '#ffffff'});
+
         this.add.image(400, 300, "detritos");
-        // this.planetas = ["planetaAmarelo", "planetaAzul", "planetaBege", "planetaRosa", "planetaTerra", "planetaVermelho"];
-        // this.aleatorio = 0;
-        this.add.image(400, 300, "planetaAzul").setScale(0.5);
+
+        this.planetas = ["planetaAmarelo", "planetaAzul", "planetaBege", "planetaRosa", "planetaTerra", "planetaVermelho"];
+        this.aleatorio = Phaser.Math.RND.between(0, 5);
+
+        this.bola = this.add.image(400, 300, this.planetas[this.aleatorio]).setScale(0.5);
+
+        this.teclado = this.input.keyboard.createCursorKeys();
 
         
     }
 
     update(){
+        if (this.teclado.up.isDown){
+            this.playerAzul.y -=10;
+        }
+        else if (this.teclado.down.isDown){
+            this.playerAzul.y +=10;
+        }
         
     }
 }
